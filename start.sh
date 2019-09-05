ff0923072127f6a805c5b34fd1bd2447008db629
Nat Tuck <nat@ferrus.net>
2019-09-05 14:35:41 -0400

    Fix service file.

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@
 export MIX_ENV=prod
 export PORT=4080
 
-echo "Stopping old copy of app, if any..."
+#echo "Stopping old copy of app, if any..."
 
-_build/prod/rel/inkfish/bin/inkfish stop || true
+#_build/prod/rel/inkfish/bin/inkfish stop || true
 
 echo "Starting app..."
 
